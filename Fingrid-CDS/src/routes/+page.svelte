6995a39656361ba846fa0619bf5434bfa6dfd4be
justyna <justynaonceagainforgothermmail@gmail.com>
2024-11-09 18:19:44 +0100

    working (lmao) timeline

--- Fingrid-CDS/src/routes/+page.svelte
+++ Fingrid-CDS/src/routes/+page.svelte
@@ -1,31 +1,6 @@
 <script>
-<<<<<<< Updated upstream
   import OfficialPosts from "./index/OfficialPosts.svelte";
 import CommunityPosts from "./index/OfficialPosts.svelte";
 </script>
 
 <OfficialPosts/>
-=======
-  import CommunityPosts from "./index/CommunityPosts.svelte";
-  import ChatComponent from "$lib/ChatComponent.svelte";
-</script>
-
-<CommunityPosts />
-
-<div class="chat-container">
-  <h2>Chat</h2>
-  <ChatComponent />
-</div>
-
-<style>
-  .chat-container {
-    max-width: 800px;
-    margin: 2rem auto;
-    padding: 0 1rem;
-  }
-  
-  h2 {
-    margin-bottom: 1rem;
-  }
-</style>
->>>>>>> Stashed changes
